d706a1e5e77029e261f95cc758b02b541781ae7f
Alaa Gamal <alaadesigner85@gmail.com>
2023-06-16 18:11:57 +0300

    adding get product by id useing http call

--- product/src/app/product/product-page/product-page.component.ts
+++ product/src/app/product/product-page/product-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { product } from 'src/app/domains/product';
 
 @Component({
@@ -6,12 +8,33 @@ import { product } from 'src/app/domains/product';
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit{
+  
   product: product={title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
   price: 109.95,
   description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
   category:"men's clothing",
   image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
 }
+productsUrl: string = "https://fakestoreapi.com/products";
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+
+  }
+  ngOnInit(): void{
+    this.route.paramMap.subscribe((param: ParamMap) => {
+      let id = parseInt(param.get('id') as string) ;
+      this.setProduct(id);
+    })
+    
+    }
+    setProduct(id: number): void{
+      this.http.get<product>(this.productsUrl + "/" + id)
+      .subscribe((product) => {
+        this.product = product;
+      })
+  }
   
+
+
+
 }
